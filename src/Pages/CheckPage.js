6af469820f1b2a0a385dbfe7b81f7c4b51b3fe02
vishal dhiman <vishaldhiman7404@gmail.com>
2023-01-14 12:46:57 +0530

    anil commit

--- src/Pages/CheckPage.js
+++ src/Pages/CheckPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CheckPage = () => {
     return (
-        <div>CheckPag bejbbee</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo et modi numquam at repellendus eligendi in veritatis harum magnam cupiditate, laudantium distinctio itaque fuga? Veritatis ipsum modi dicta unde assumenda!</div>
     )
 }
 
